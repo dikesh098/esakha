5e24bad095c2e4dab7111f8d08f993d516e41c40
uk <65726936+UjwalThakkar@users.noreply.github.com>
2025-01-14 15:11:19 +0530

    done with env file

--- firebase_config.js
+++ firebase_config.js
@@ -1,9 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-console.log(import.meta.env.VITE_API_KEY);
-console.log(import.meta.env.VITE_APP_ID);
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
